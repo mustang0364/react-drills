791508da66aa7c5ee454481c6c45ba974e80a3bd
abraham martinez <abrahammartinez@abrahams-MacBook-Pro.local>
2018-06-25 15:59:33 -0700

    number2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  
+  
+  constructor(){
+    super();
+    this.state={
+     foods2:['pizza','soup','cheese']
+    }
+      
+    
+  }
+  
+ 
+  
+  
+
+  
   render() {
+    let food = this.state.foods2.map((element, index)=>{
+      return (
+        <h2 key ={index}>{element}</h2>
+      )
+    })
+
+
+
+
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      {food}
+      
       </div>
     );
   }
