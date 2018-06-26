76745f6f88f6c51879c0e235fa73ea10a41749ca
abraham martinez <abrahammartinez@abrahams-MacBook-Pro.local>
2018-06-25 20:43:39 -0700

    number4

--- app-4/src/App.test.js
+++ app-4/src/App.test.js
@@ -1,8 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React, { Component } from 'react';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      username: "",
+      password: ""
+    };
+
+    this.handleLogin = this.handleLogin.bind( this );
+  }
+
+  handleUsernameChange( name ) {
+    this.setState({ username: name });
+  }
+
+  handlePasswordChange( pass ) {
+    this.setState({ password: pass });
+  }
+
+  handleLogin() {
+    alert(`Username: ${ this.state.username } Password: ${ this.state.password }`);
+  }
+
+  render() {
+    return (
+      <div>
+        <input onChange={ (e) => this.handleUsernameChange( e.target.value ) } type="text"/>
+        <input onChange={ (e) => this.handlePasswordChange( e.target.value ) } type="text"/>
+        <button onClick={ this.handleLogin }>Login</button>
+      </div>
+    )
+  }
+}
+
+export default Login;
